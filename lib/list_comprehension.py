de5cb2e92ccdfee5e7ee53ba476dcd121ab410d9
leon <onditileon21@gmail.com>
2024-03-02 18:04:03 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
-
+    even_num = [n for n in num_list if n % 2 == 0]
+    return even_num
+print(return_evens([0,1,2,3,4]))
+    
 def make_exclamation(sentence_list):
-    pass
+    sentence = [i + '!' for i in sentence_list]
+    return sentence
+print(make_exclamation("Hello there," "miss"))
